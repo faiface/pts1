236b6f8c9cb4a1e4820a980bf82c1b7e3c162217
faiface <faiface@ksp.sk>
2017-03-14 17:17:28 +0100

    add points, reduce, junior and input handling

--- pts1.py
+++ pts1.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 def protected(password):
     def decorator(f):
         def wrapper(*args, **kwargs):
@@ -15,6 +13,35 @@ def protected(password):
             f(*args, **kwargs)
         return decorator
 
+class Player:
+    def __init__(self, name, score, junior):
+        self.name = name
+        self.score = score
+        self.junior = junior
+
+password = "principlesandpracticeofinfectiousdiseases"
+
+@protected(password)
+def points(players, name, score):
+    for player in players:
+        if player.name == name:
+            player.score += score
+            break
+    else:
+        players.append(Player(name, score, False))
+
+@protected(password)
+def reduce(players, score):
+    for player in players:
+        player.score -= score
+
+@protected(password)
+def junior(players, name):
+    for player in players:
+        if player.name == name:
+            player.junior = True
+            break
+
 def ranking(players):
     for player in sorted(players, key=lambda p: p.score, reverse=True):
         print("{:4}: {} ({})".format(
@@ -28,3 +55,23 @@ def ranking_junior(players):
 
 if __name__ == "__main__":
     players = []
+
+    while True:
+        inp = input().split()
+        command = inp[0]
+
+        if command == "points":
+            points(players, inp[1], int(inp[2]))
+        elif command == "reduce":
+            reduce(players, int(inp[1]))
+        elif command == "junior":
+            junior(players, inp[1])
+        elif command == "ranking":
+            if len(inp) > 1 and inp[1] == "junior":
+                ranking_junior(players)
+            else:
+                ranking(players)
+        elif command == "quit":
+            break
+        else:
+            print("Unknown command: {}".format(command))
